9be00a6c9412d649c14ce547b01ca726497aa071
キリスト教の <32336098+christ668@users.noreply.github.com>
2020-08-22 12:58:48 +0700

    code gen graph

--- db.ts
+++ db.ts
@@ -1,49 +1,139 @@
-export const messages = [
+// export const messages = [
+// {
+//   id: "1",
+//   content: "Test 1",
+//   createdAt: new Date(Date.now() - 60 * 1000 * 1000),
+// },
+// {
+//   id: "2",
+//   content: "Test 2",
+//   createdAt: new Date(Date.now() - 2 * 60 * 1000 * 1000),
+// },
+// {
+//   id: "3",
+//   content: "Test 3",
+//   createdAt: new Date(Date.now() - 24 * 60 * 1000 * 1000),
+// },
+// {
+//   id: "4",
+//   content: "Test 4",
+//   createdAt: new Date(Date.now() - 14 * 24 * 60 * 1000 * 1000),
+// },
+// ];
+
+// export const chats = [
+// {
+//   id: "1",
+//   name: "roby mis",
+//   picture: "https://randomuser.me/api/portraits/thumb/men/1.jpg",
+//   messages: ["1"],
+// },
+// {
+//   id: "2",
+//   name: "david mis",
+//   picture: "https://randomuser.me/api/portraits/thumb/men/2.jpg",
+//   messages: ["2"],
+// },
+// {
+//   id: "3",
+//   name: "fani mis",
+//   picture: "https://randomuser.me/api/portraits/thumb/women/1.jpg",
+//   messages: ["3"],
+// },
+// {
+//   id: "4",
+//   name: "hendry mis",
+//   picture: "https://randomuser.me/api/portraits/thumb/men/3.jpg",
+//   messages: ["4"],
+// },
+// ];
+
+export type Message = {
+  id: string;
+  content: string;
+  createdAt: Date;
+};
+export type Chat = {
+  id: string;
+  name: string;
+  picture: string;
+  messages: string[];
+};
+
+export const messages: Message[] = [
   {
     id: "1",
-    content: "Test 1",
-    createdAt: new Date(Date.now() - 60 * 1000 * 1000),
-  },
-  {
-    id: "2",
-    content: "Test 2",
-    createdAt: new Date(Date.now() - 2 * 60 * 1000 * 1000),
-  },
-  {
-    id: "3",
-    content: "Test 3",
-    createdAt: new Date(Date.now() - 24 * 60 * 1000 * 1000),
-  },
-  {
-    id: "4",
-    content: "Test 4",
-    createdAt: new Date(Date.now() - 14 * 24 * 60 * 1000 * 1000),
+    content: "You on your way?",
+    createdAt: new Date(new Date("1-1-2019").getTime() - 60 * 1000 * 1000),
   },
 ];
-
-export const chats = [
+export const chats: Chat[] = [
   {
     id: "1",
-    name: "roby mis",
+    name: "Ethan Gonzalez",
     picture: "https://randomuser.me/api/portraits/thumb/men/1.jpg",
     messages: ["1"],
   },
-  {
-    id: "2",
-    name: "david mis",
-    picture: "https://randomuser.me/api/portraits/thumb/men/2.jpg",
-    messages: ["2"],
-  },
-  {
-    id: "3",
-    name: "fani mis",
-    picture: "https://randomuser.me/api/portraits/thumb/women/1.jpg",
-    messages: ["3"],
-  },
-  {
-    id: "4",
-    name: "hendry mis",
-    picture: "https://randomuser.me/api/portraits/thumb/men/3.jpg",
-    messages: ["4"],
-  },
 ];
+
+export const resetDb = () => {
+  messages.splice(
+    0,
+    Infinity,
+    ...[
+      {
+        id: "1",
+        content: "Test 1",
+        createdAt: new Date(Date.now() - 60 * 1000 * 1000),
+      },
+      {
+        id: "2",
+        content: "Test 2",
+        createdAt: new Date(Date.now() - 2 * 60 * 1000 * 1000),
+      },
+      {
+        id: "3",
+        content: "Test 3",
+        createdAt: new Date(Date.now() - 24 * 60 * 1000 * 1000),
+      },
+      {
+        id: "4",
+        content: "Test 4",
+        createdAt: new Date(Date.now() - 14 * 24 * 60 * 1000 * 1000),
+      },
+    ]
+  );
+
+  chats.splice(
+    0,
+    Infinity,
+    ...[
+      {
+        id: "1",
+        name: "roby mis",
+        picture: "https://randomuser.me/api/portraits/thumb/men/1.jpg",
+        messages: ["1"],
+      },
+      {
+        id: "2",
+        name: "david mis",
+        picture: "https://randomuser.me/api/portraits/thumb/men/2.jpg",
+        messages: ["2"],
+      },
+      {
+        id: "3",
+        name: "fani mis",
+        picture: "https://randomuser.me/api/portraits/thumb/women/1.jpg",
+        messages: ["3"],
+      },
+      {
+        id: "4",
+        name: "hendry mis",
+        picture: "https://randomuser.me/api/portraits/thumb/men/3.jpg",
+        messages: ["4"],
+      },
+    ]
+  );
+};
+
+resetDb();
